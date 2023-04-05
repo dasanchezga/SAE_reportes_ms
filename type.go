fe9fe688548e4a3f763c073bf64788acedbecea9
dasanchezga <dasanchezga@unal.edu.co>
2023-04-05 12:14:33 -0500

    Correciones en el struct

--- type.go
+++ type.go
@@ -2,26 +2,18 @@ package main
 
 import "time"
 
-type Reportes struct {
-	//Formularios
-	ID                int    `json:"id"`
-	Nombre_Formulario string `json:"nombre_form"`
-	Nivel             string `json:"nivel"`
-	Documento         int  `json:"documento"`
-	Tipologia         string `json:"tipologia"`
-
-	//Tutorias
-	Usuario_Estudiante string    `json:"usuario_estudiante"`
-	Usuario_Tutor      string    `json:"usuario_tutor"`
-	Fecha              time.Time `json:"fecha"`
-	Objetivo           string    `json:"objetivo"`
-	Acuerdo            string    `json:"acuerdo"`
-
-	//Observaciones
+type Lista struct {
+	Lugar    string `json:"lugar"`
+	Estado   string `json:"estado"`
+	Objetivo string `json:"objetivo"`
+	Acuerdo  string `json:"acuerdo"`
+}
+type Observaciones struct {
 	Fecha_Obs   string `json:"fecha_obs"`
 	Descripcion string `json:"descripcion"`
+}
 
-	//Remisiones
+type Remisiones struct{
 	Usuario_Estudiante1 string `json:"usuario_estudiante1"`
 	Usuario_Tutor1      string `json:"usuario_tutor1"`
 	Tipo_Remision       string `json:"tipo_remision"`
@@ -33,6 +25,37 @@ type Reportes struct {
 	Justificacion       string `json:"justificacion"`
 	Fecha_Solicitud     string `json:"fecha_solicitud"`
 	Fecha_Aprobacion    string `json:"fecha_aprobacion"`
+
+}
+
+type Reportes struct {
+	//Usuarios
+	Usuario_Estudiante0 string `json:"usuario_estudiante0"`
+	Usuario_Tutor0      string `json:"usuario_tutor0"`
+	Programa            string `json:"programa"`
+	Departamento        string `json:"departamento"`
+	Facultad            string `json:"facultad"`
+
+	//Formularios
+	ID                int    `json:"id"`
+	Nombre_Formulario string `json:"nombre_form"`
+	Nivel             string `json:"nivel"`
+	Documento         int    `json:"documento"`
+	Tipologia         string `json:"tipologia"`
+
+	//Tutorias
+	Usuario_Estudiante string    `json:"usuario_estudiante"`
+	Usuario_Tutor      string    `json:"usuario_tutor"`
+	Estado_Tutor       string    `json:"estado_tutor"`
+	Fecha              time.Time `json:"fecha"`
+
+	Lista []Lista `json:"lista"`
+
+	//Observaciones
+	Observaciones []Observaciones `json:"observaciones"`
+
+	//Remisiones
+	Remisiones []Remisiones `json:"remisiones"`
 }
 
 var reporte []Reportes
